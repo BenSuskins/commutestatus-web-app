558f6322b426de035d47d136a1b93e2d7560ff4c
Ben Suskins <suskinsdevelopment@gmail.com>
2020-04-17 12:01:18 +0100

    Switched to services rather than API calls in VUEX

---
+++ src/services/CommuteStatusService.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { getInstance } from "../auth/index";
+
+const apiClient = axios.create({
+  baseURL: `http://localhost:8080/api/v1/`,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  },
+  timeout: 10000
+});
+
+export default {
+  getStatuses() {
+    const authservice = getInstance();
+    return authservice.getIdTokenClaims().then(claims => {
+      return apiClient.get("secure/commutestatus", {
+        headers: {
+          Authorization: "Bearer " + claims.__raw
+        }
+      });
+    });
+  }
+};
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import axios from "axios";
-import { getInstance } from "../auth/index";
+import UserService from "../services/UserService";
+import CommuteStatusService from "../services/CommuteStatusService";
 
 Vue.use(Vuex);
 
@@ -38,55 +38,33 @@ export default new Vuex.Store({
       context.commit("SET_TO_HOME", []);
       context.commit("SET_ERROR", []);
 
-      //Set access token
-      const authservice = getInstance();
-      authservice.getIdTokenClaims().then(claims => {
-        let options = {
-          headers: {
-            Authorization: "Bearer " + claims.__raw
-          }
-        };
-
-        //Make api call
-        axios
-          .get("http://localhost:8080/api/v1/secure/commutestatus", options)
-          .then(response => {
-            context.commit("SET_STATUS", "");
-            context.commit("SET_TO_WORK", response.data.toWork);
-            context.commit("SET_TO_HOME", response.data.toHome);
-          })
-          .catch(error => {
-            console.log(error);
-            context.commit("SET_STATUS", "errored");
-            context.commit("SET_ERROR", error);
-          });
-      });
+      //Call commute status service
+      CommuteStatusService.getStatuses()
+        .then(response => {
+          context.commit("SET_STATUS", "");
+          context.commit("SET_TO_WORK", response.data.toWork);
+          context.commit("SET_TO_HOME", response.data.toHome);
+        })
+        .catch(error => {
+          console.log(error);
+          context.commit("SET_STATUS", "errored");
+          context.commit("SET_ERROR", error);
+        });
     },
     fetchUser(context) {
       //Clear state
       context.commit("SET_USER", {});
 
-      //Set access token
-      const authservice = getInstance();
-      authservice.getIdTokenClaims().then(claims => {
-        let options = {
-          headers: {
-            Authorization: "Bearer " + claims.__raw
-          }
-        };
-
-        //Make api call
-        axios
-          .get("http://localhost:8080/api/v1/secure/user", options)
-          .then(response => {
-            context.commit("SET_USER", response.data);
-          })
-          .catch(error => {
-            console.log(error);
-            context.commit("SET_STATUS", "errored");
-            context.commit("SET_ERROR", error);
-          });
-      });
+      //Call user service
+      UserService.getUser()
+        .then(response => {
+          context.commit("SET_USER", response.data);
+        })
+        .catch(error => {
+          console.log(error);
+          context.commit("SET_STATUS", "errored");
+          context.commit("SET_ERROR", error);
+        });
     }
   },
   getters: {

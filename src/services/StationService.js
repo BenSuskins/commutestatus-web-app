04d01dacb021b4442871de86f743386bf8ddc934
Ben Suskins <suskinsdevelopment@gmail.com>
2020-04-22 16:42:16 +0100

    Get api URL from env

--- src/services/StationService.js
+++ src/services/StationService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: `http://localhost:8080/api/v1/`,
+  baseURL: process.ENV.VUE_APP_API_ROOT | "http://localhost:8080/api/v1/",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json"
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getInstance } from "../auth/index";
 
 const apiClient = axios.create({
-  baseURL: `http://localhost:8080/api/v1/`,
+  baseURL: process.ENV.VUE_APP_API_ROOT | "http://localhost:8080/api/v1/",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json"
